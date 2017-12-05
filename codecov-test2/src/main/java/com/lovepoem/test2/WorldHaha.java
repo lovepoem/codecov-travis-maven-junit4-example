862692f1922598403e2ca1e2f2d8207ac80d3981
WangXin <lovepoem@hotmail.com>
2017-12-05 11:12:57 +0800

    three_test_case

---
+++ codecov-test2/src/main/java/com/lovepoem/test2/WorldHaha.java
@@ -0,0 +1,13 @@
+package com.lovepoem.test2;
+
+public class WorldHaha {
+	
+	public String getMessage(boolean bigger) {
+		if (bigger) {
+			return "WorldHaha big!";
+		} else {
+			return "WorldHaha small!";
+		}
+	}
+
+}
